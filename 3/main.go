51060d7038bedcb8a810f6e7414ab68d267efcd4
John Hobbs <john@velvetcache.org>
2017-12-18 22:20:32 -0600

    Puzzle 3, A - Brute force spiral mapping.

--- 3/main.go
+++ 3/main.go
@@ -3,6 +3,69 @@ package main
 func main() {
 }
 
+const (
+	RIGHT = iota
+	UP
+	LEFT
+	DOWN
+)
+
 func countSteps(src int) int {
 	return 0
+}
+
+func getSpiralLocation(src int) (x, y int) {
+	x = -1
+	y = 0
+	w := 0
+	direction := RIGHT
+
+	for i := 1; i <= src; i++ {
+
+		if x == w && y == -1*w {
+			w = w + 1
+		}
+
+		if direction == RIGHT {
+			if x == w {
+				direction = UP
+			} else {
+				x = x + 1
+				continue
+			}
+		}
+
+		if direction == UP {
+			if y == w {
+				direction = LEFT
+			} else {
+				y = y + 1
+				continue
+			}
+		}
+
+		if direction == LEFT {
+			if x == -1*w {
+				direction = DOWN
+			} else {
+				x = x - 1
+				continue
+			}
+		}
+
+		if direction == DOWN {
+			if y == -1*w {
+				direction = RIGHT
+			} else {
+				y = y - 1
+				continue
+			}
+		}
+
+		if direction == RIGHT {
+			x = x + 1
+		}
+	}
+
+	return
 }
--- 3/main_test.go
+++ 3/main_test.go
@@ -21,3 +21,53 @@ func TestKnownMoves(t *testing.T) {
 		}
 	}
 }
+
+func TestSpiralLocation(t *testing.T) {
+	/*
+		   37  36  35  34  33  32  31
+		   38  17  16  15  14  13  30
+		   39  18   5   4   3  12  29
+		   40  19   6   1   2  11  28
+		   41  20   7   8   9  10  27
+		   42  21  22  23  24  25  26
+		   43  44  45  46  47  48  49
+
+		   1 is at 0, 0
+			 ring 1 is 2-9, 7
+			 ring 2 is 10-25, 15
+			 ring 3 is 26-49, 23
+
+			 right
+			 up
+			 left
+			 left
+			 down
+			 down
+			 right
+			 right
+	*/
+
+	expected := [][]int{
+		[]int{1, 0, 0},
+		[]int{2, 1, 0},
+		[]int{3, 1, 1},
+		[]int{4, 0, 1},
+		[]int{5, -1, 1},
+		[]int{6, -1, 0},
+		[]int{7, -1, -1},
+		[]int{8, 0, -1},
+		[]int{9, 1, -1},
+		[]int{10, 2, -1},
+		[]int{11, 2, 0},
+		[]int{12, 2, 1},
+		[]int{13, 2, 2},
+		[]int{14, 1, 2},
+	}
+
+	for _, expect := range expected {
+		x, y := getSpiralLocation(expect[0])
+		if x != expect[1] || y != expect[2] {
+			t.Errorf("Wrong location for %d. Expected %d,%d got %d,%d", expect[0], expect[1], expect[2], x, y)
+		}
+	}
+}
