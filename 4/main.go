ca42c5bc4def1f3f4b1aa360bb395fb0a65f1934
John Hobbs <john@velvetcache.org>
2017-12-19 17:56:51 -0600

    Puzzle 4, A - Solution

--- 4/main.go
+++ 4/main.go
@@ -1,8 +1,43 @@
 package main
 
+import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
+)
+
 func main() {
+	file, err := os.Open("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	valid := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if validPassphrase(scanner.Text()) {
+			valid += 1
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Part A:", valid)
 }
 
 func validPassphrase(pp string) bool {
-	return false
+	seen := map[string]bool{}
+
+	for _, word := range strings.Fields(pp) {
+		if _, ok := seen[word]; ok {
+			return false
+		}
+		seen[word] = true
+	}
+
+	return true
 }
