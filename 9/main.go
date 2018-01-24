a5b9b37153f5b48a4ad83d4e9b09a8a6c4dd3560
John Hobbs <john@velvetcache.org>
2018-01-23 22:10:45 -0600

    Puzzle 9, A - Solution

--- 9/main.go
+++ 9/main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
 )
 
 func main() {
+	contents, err := ioutil.ReadFile("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	root := ProcessStream(bytes.NewBuffer(contents))
+	fmt.Println("Part A:", root.TotalScore())
 }
 
 type Group struct {
@@ -14,13 +25,73 @@ type Group struct {
 }
 
 func ProcessStream(r io.ByteReader) Group {
-	return Group{nil, []*Group{}, 0}
+	// { - Opens group
+	// } - Closes group
+	// < - Opens garbage
+	// > - Closes garbage
+	// ! - Skip next char (in garbage)
+
+	root_group := Group{nil, []*Group{}, 0}
+	current_group := &root_group
+	skip_char := false
+	garbage_opened := false
+
+	for {
+		b, err := r.ReadByte()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+
+		if garbage_opened {
+			if skip_char {
+				skip_char = false
+				continue
+			}
+
+			if b == '>' {
+				garbage_opened = false
+			}
+
+			if b == '!' {
+				skip_char = true
+			}
+
+			continue
+		}
+
+		if b == '<' {
+			garbage_opened = true
+		}
+
+		if b == '{' {
+			new_group := Group{current_group, []*Group{}, current_group.Score + 1}
+			current_group.Children = append(current_group.Children, &new_group)
+			current_group = &new_group
+		}
+
+		if b == '}' {
+			current_group = current_group.Parent
+		}
+	}
+
+	return root_group
 }
 
 func (g Group) Count() int {
-	return 0
+	total := len(g.Children)
+	for _, g := range g.Children {
+		total += g.Count()
+	}
+	return total
 }
 
 func (g Group) TotalScore() int {
-	return 0
+	total := g.Score
+	for _, g := range g.Children {
+		total += g.TotalScore()
+	}
+	return total
 }
