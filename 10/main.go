bc0664a5bc695b7cb46f06944d565fb2c6728b9b
John Hobbs <john@velvetcache.org>
2018-01-25 11:36:47 -0600

    Puzzle 10, A - Solution

--- 10/main.go
+++ 10/main.go
@@ -1,6 +1,30 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := ioutil.ReadFile("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	kh := New(256)
+	for _, step := range strings.Split(string(input), ",") {
+		i, err := strconv.Atoi(step)
+		if err != nil {
+			log.Fatal(err)
+		}
+		kh.Knot(i)
+	}
+
+	fmt.Println("Part A:", kh.List[0]*kh.List[1])
+}
 
 type KnotHash struct {
 	Size     int
@@ -18,4 +42,16 @@ func New(size int) *KnotHash {
 }
 
 func (kh *KnotHash) Knot(length int) {
+	// - Reverse the order of that length of elements in the list, starting with the element at the current position.
+	tmp := make([]int, length)
+	for i := 0; i < length; i++ {
+		tmp[length-i-1] = kh.List[(kh.Position+i)%kh.Size]
+	}
+	for i := 0; i < length; i++ {
+		kh.List[(kh.Position+i)%kh.Size] = tmp[i]
+	}
+	// - Move the current position forward by that length plus the skip size.
+	kh.Position = (kh.Position + length + kh.SkipSize) % kh.Size
+	// - Increase the skip size by one.
+	kh.SkipSize += 1
 }
